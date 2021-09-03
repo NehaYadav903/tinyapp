1a000a9181151972c198510cb2f71f80b02cd0ba
Neha Yadav <nehayadav903@gmail.com>
2021-09-02 23:35:58 -0400

    Add the login and logut feature

--- express_server.js
+++ express_server.js
@@ -1,93 +1,117 @@
 const express = require("express");
 const app = express();
 
-//middleware
-const bodyParser = require('body-parser')
-
-// default port 8080
+//Default port 8080
 const PORT = 8080; 
 
-//Use EJS as its templating engine
-app.set("view engine", "ejs");
-app.use(express.json());
-app.use(bodyParser.urlencoded({extended: true}));
+//Middleware
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-// example of users' url database
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
+//Example of users' url database
 const urlDatabase = {
   "b2xVn2": {longURL: "http://www.lighthouselabs.ca" },
   "9sm5xK": {longURL: "http://www.google.com" }
 };
-//CREATE random string for shortURL
 
+//CREATE random string for shortURL
 function generateRandomString() {
-  let chars = "0123456789abcdefghijklmnopqrstuvwxyz";
+  let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
   let result = "";
   for (let i = 0; i < 6; i ++) {
     result += chars.charAt(Math.floor(Math.random() * 36));
   }
   return result;
 }
-// to display urls index page
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
 
-// to display urls index page
-app.get("/urls", (req, res) => { 
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-// display short URL
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.body.longURL };
-  res.render("urls_show", templateVars);
-});
+//CREATE a new short URL
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  let randomString = generateRandomString();
+  urlDatabase[randomString] = req.body.longURL;
+  res.redirect(`/urls/${randomString}`);
 });
 
-// to redirect to long URL
-app.get("/u/:shortURL", (req, res) => {
-  const longUrl = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(longURL);
-});
-
-// deleting urls
+//DELETE a URL from the database
 app.post("/urls/:shortURL/delete", (req, res) => {
-  let short = req.params.shortURL;
-  delete urlDatabase[short];
+  delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
 
-app.post("/urls/:shortURL/edit", (req, res) => {
-  let short = req.params.shortURL;
-  urlDatabase[short].longURL = req.body.longURL;
-  res.redirect(`/urls`);
+//UPDATE a URL, helper function
+const updateURL = (id, content) => {
+  urlDatabase[id] = content;
+};
+
+//POST to UPDATE the url in the Database
+app.post("/urls/:id", (req, res) => {
+  const urlID = req.params.id;
+  const longURL = req.body.longURL;
+  
+  updateURL(urlID, longURL);
+  
+  res.redirect(`/urls/${urlID}`);
 });
 
-// Log the POST request body to the console
-
-app.post("/urls", (req, res) => {
-  user_id = req.session["user_id"];
-  if (req.session["user_id"]) {
-    let short = generateRandomString();
-    urlDatabase[short] = {
-      longURL: req.body.longURL,
-      userID: user_id };
-    res.redirect(`/urls/${short}`);
-  
-  } else { 
-    res.redirect("/urls");
+//POST to handle the /login in your Express server
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  if (username) {
+    res.cookie('username', username);
+    res.redirect('/urls');
   }
+});
+
+//POST to handle /logout
+app.post("/logout", (req, res) => {
+  //console.log(req.body);
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
+//To redirect to long URL
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
+//New url is created
+app.get("/urls/new", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"]
+  };
+  res.render("urls_new", templateVars);
+});
+
+//To display urls index page
+app.get("/urls", (req, res) => {
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+  res.render("urls_index", templateVars);
+});
+
+//Add shortURL and longURL values to the list of URLs on the myURLs page
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"]
+  };
+  res.render("urls_show", templateVars);
 });
 
 //JSON string representing the entire urlDatabase object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
+
 
